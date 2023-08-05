064c728292e9b2900ee89993b1d67dc46c65aa5f
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-08-05 05:04:30 +0300

    Update OpenSit.java

--- PaperMC 1.20.1/src/main/java/illa4257/opensit/OpenSit.java
+++ PaperMC 1.20.1/src/main/java/illa4257/opensit/OpenSit.java
@@ -1,5 +1,6 @@
 package illa4257.opensit;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OpenSit extends JavaPlugin {
@@ -7,5 +8,10 @@ public final class OpenSit extends JavaPlugin {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new OpenSitListener(), this);
         getCommand("sit").setExecutor(new SitCommand());
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll(this);
     }
 }
