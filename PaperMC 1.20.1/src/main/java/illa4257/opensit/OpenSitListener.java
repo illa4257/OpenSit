975ece07fff2aa4c5ca813ecd3a4bd9b67d0f0a3
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-09-18 00:27:52 +0300

    Fix #3

--- PaperMC 1.20.1/src/main/java/illa4257/opensit/OpenSitListener.java
+++ PaperMC 1.20.1/src/main/java/illa4257/opensit/OpenSitListener.java
@@ -6,55 +6,22 @@ import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Slab;
 import org.bukkit.block.data.type.Stairs;
+import org.bukkit.block.data.type.TrapDoor;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.util.Vector;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OpenSitListener implements Listener {
-    @EventHandler
-    public void onDismount(final EntityDismountEvent event) {
-        if (!(event.getEntity() instanceof Player))
-            return;
-        final Entity e = event.getDismounted();
-        if (e instanceof BlockDisplay)
-            if (e.getScoreboardTags().contains("sit")) {
-                event.getEntity().teleport(event.getEntity().getLocation().add(0, 1, 0));
-                e.remove();
-            } else if (e.getScoreboardTags().contains("sit2")) {
-                event.getEntity().teleport(event.getEntity().getLocation().add(0, 1.5, 0));
-                e.remove();
-            }
-    }
-
-    @EventHandler
-    public void onInteract(final PlayerInteractEvent event) {
-        if (event.getItem() != null || event.getClickedBlock() == null || event.getAction().isLeftClick() || !event.getPlayer().hasPermission("OpenSit.SitClick"))
-            return;
-        final BlockData d = event.getClickedBlock().getState().getBlockData();
-        if (d instanceof Slab || d instanceof Stairs) {
-            final boolean t = d instanceof Slab ? ((Slab) d).getType() != Slab.Type.BOTTOM : ((Stairs) d).getHalf() == Bisected.Half.TOP;
-            final Location l = event.getClickedBlock().getLocation();
-            final BlockDisplay b = (BlockDisplay) l.getWorld().spawnEntity(new Location(l.getWorld(), l.getX() + .5, l.getY() + (t ? .8 : .3), l.getZ() + .5), EntityType.BLOCK_DISPLAY);
-            b.addScoreboardTag(d instanceof Slab || t ? "sit" : "sit2");
-            b.addPassenger(event.getPlayer());
-        }
-    }
-
-    @EventHandler
-    public void onPlayerJoin(final PlayerJoinEvent event) {
-        final Entity e = event.getPlayer().getVehicle();
-        if (e instanceof BlockDisplay && (e.getScoreboardTags().contains("sit") || e.getScoreboardTags().contains("sit2")) && !e.getLocation().getBlock().isSolid())
-            e.remove();
-    }
-
-    @EventHandler
-    public void onBreakBlock(final BlockBreakEvent event) {
-        final Block b = event.getBlock();
-        final Location l = b.getLocation();
+    public static void removeSitsInBlock(final Location l) {
         final double sy = l.getBlockY() - .1, ey = l.getBlockY() + 1;
         final int sx = l.getBlockX(), sz = l.getBlockZ(), ex = sx + 1, ez = sz + 1;
         for (final Entity e : l.getChunk().getEntities())
@@ -62,9 +29,55 @@ public class OpenSitListener implements Listener {
                 final Location l2 = e.getLocation();
                 if (
                         l2.getY() > sy && l2.getY() <= ey &&
-                        l2.getX() >= sx && l2.getX() <= ex &&
-                        l2.getZ() >= sz && l2.getZ() <= ez
+                                l2.getX() >= sx && l2.getX() <= ex &&
+                                l2.getZ() >= sz && l2.getZ() <= ez
                 ) e.remove();
             }
     }
+
+    @EventHandler
+    public void onDismount(final EntityDismountEvent event) {
+        final Entity e = event.getDismounted();
+        if (e instanceof BlockDisplay) {
+            if (e.getScoreboardTags().contains("sit"))
+                event.getEntity().teleport(event.getEntity().getLocation().add(0, 1, 0));
+            else if (e.getScoreboardTags().contains("sit2"))
+                event.getEntity().teleport(event.getEntity().getLocation().add(0, 1.5, 0));
+            else
+                return;
+            if (e.getPassengers().isEmpty())
+                e.remove();
+        }
+    }
+
+    @EventHandler
+    public void onInteract(final PlayerInteractEvent event) {
+        final Block b = event.getClickedBlock();
+        if (event.getItem() != null || b == null || event.getAction().isLeftClick() || !event.getPlayer().hasPermission("OpenSit.SitClick"))
+            return;
+        final Location l = b.getLocation();
+        final Block b2 = b.getWorld().getBlockAt(l.getBlockX(), l.getBlockY() + 1, l.getBlockZ());
+        if (b2.isSolid() && !(b2.getState().getBlockData() instanceof TrapDoor))
+            return;
+        final BlockData d = b.getState().getBlockData();
+        if (d instanceof Slab || d instanceof Stairs) {
+            final boolean t = d instanceof Slab ? ((Slab) d).getType() != Slab.Type.BOTTOM : ((Stairs) d).getHalf() == Bisected.Half.TOP;
+            final BlockDisplay r = (BlockDisplay) l.getWorld().spawnEntity(new Location(l.getWorld(), l.getX() + .5, l.getY() + (t ? .8 : .3), l.getZ() + .5), EntityType.BLOCK_DISPLAY);
+            r.addScoreboardTag(d instanceof Slab || t ? "sit" : "sit2");
+            r.addPassenger(event.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(final PlayerJoinEvent event) {
+        final Entity e = event.getPlayer().getVehicle();
+        if (e instanceof BlockDisplay && (e.getScoreboardTags().contains("sit") || e.getScoreboardTags().contains("sit2"))) {
+            final Location l = new Location(e.getWorld(), e.getLocation().getX(), e.getLocation().getY() + .2, e.getLocation().getZ());
+            final Block b = l.getWorld().getBlockAt(l.getBlockX(), l.getBlockY() + 1, l.getBlockZ());
+            if (!l.getBlock().isSolid() || (b.isSolid() && !(b.getState().getBlockData() instanceof TrapDoor)))
+                e.remove();
+        }
+    }
+
+    @EventHandler public void onBreakBlock(final BlockBreakEvent event) { removeSitsInBlock(event.getBlock().getLocation()); }
 }
